bd7ee8621c432134c7732f9ddd2f9350c6d7fd4d
Esth0902 <esther.stassin@gmail.com>
2024-10-12 12:38:50 +0200

    Réorganisation

---
+++ CurrentAccount.cs
@@ -0,0 +1,23 @@
+﻿namespace Bank;
+
+// 2. Créer une classe "CurrentAccount" qui permet la gestion d'un compte courant implémentant
+// les propriétés publiques string Number, double Balance (lecture seule), double CreditLine, Person Owner
+// les méthodes publiques void Withdraw(double amount), void Deposit(double amount)
+
+public class CurrentAccount
+{
+    public string Number { get; set; }
+    public double Balance { get; private set; }
+    public double CreditLine { get; set; }
+    public Person Owner { get; }
+
+    public void Withdraw(double amount)
+    {
+        
+    }
+
+    public void Deposit(double amount)
+    {
+        
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,11 @@
+﻿namespace Bank;
+
+// 1. Créer une classe "Person" implémentant les propriétés publiques
+// string FirstName, string LastName, DateTime BirthDate
+public class Person
+{
+    public int ID { get; init; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+}
